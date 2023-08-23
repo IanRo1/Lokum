53b4d1e1ff545ae7f58aa599cd93711f5659d8c7
Ian Ro <ianrogers2422@gmail.com>
2023-08-23 19:21:50 -0400

    session name progress

--- src/session.js
+++ src/session.js
@@ -4,14 +4,26 @@ import {Link} from 'react-router-dom';
 
 function Session(){
   const [sessionName, setSessionName] = useState("");
+  const [hiddName, sethiddName] = useState([]);
+  const [hiddButton, sethiddButton] = useState([]);
 
+  const createName=()=>{
+    sethiddName(<input className='hiddName' type="text" id='0'></input>)
+    sethiddButton(<button className='submitBtn' id='0' onClick={deleteName}>Submit</button>)
+  }
+
+  const deleteName=()=>{
+    sethiddName(hiddName.filter((x) => x.id === '0'))
+    sethiddButton(hiddButton.filter((x) =>x.id === '0'))
+  }
 
   return (
     <div className='profile'>
         <p className='profName'>'s Profile</p>
         <div className='sessContainer'>
         <p className='newSess'>Click to Start a New Session</p>
-        <div className="sessIcon">+</div>
+        <button className="sessIcon" onClick={createName}>+</button>
+        <div className='secretTxt'>{hiddName}{hiddButton}</div>
         </div>
         <div className='profileCont'></div>
         <Link to='home'>
