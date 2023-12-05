a6177a364affc202c19dba2335b5aeb1dd43a2e5
Ian Ro <ianrogers2422@gmail.com>
2023-12-05 18:54:28 -0500

    progress in savedSess

--- src/savedSession.js
+++ src/savedSession.js
@@ -4,11 +4,13 @@ import {Link} from 'react-router-dom';
 
 
 function savedSess(props){
+    const sessionNames =[props.sessionName1.map(ssnName => {
+        return <div key={ssnName.key} className='ssnTxt'>{ssnName.text}</div>})][0];
 
     return(
      <div className='savedDB'>
         <div className='sDB1'>
-            {props.sessionName1}
+            {sessionNames}
         </div>
      <Link to='session'><button className='return'></button></Link>
      </div>
